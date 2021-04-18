859d0582cd31bbd0dc150620f93af7f188d536ea
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-04-18 14:22:30 -0300

    adicionando exercicios

---
+++ Introdução/Bloco 07: Introdução à JavaScript ES6 e Testes Unitários/7.2 - JavaScript ES6 - Objects/exercicios/exercises2.js
@@ -0,0 +1,45 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+// 1 - Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+const addTurn = (obj, key, value) => {obj[key] = value;};
+addTurn(lesson2, 'turno', 'manhã');
+//2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+const listKey = (obj) => Object.keys(obj);
+console.log(listKey(lesson1));
+//3 - Crie uma função para mostrar o tamanho de um objeto.
+const sizeObj = (obj) => Object.keys(obj).length;
+console.log(sizeObj(lesson2));
+//4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+const listValue = (obj) => Object.values(obj);
+console.log(listValue(lesson1));
+//5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);
+//6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+const getNumberOfStudents = (obj) => {
+    let total = 0;
+    const array = Object.keys(obj);
+    for (index in array) {
+      total += obj[array[index]].numeroEstudantes;
+    }
+    return total;
+  };
+  console.log(getNumberOfStudents(allLessons));
+//7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
