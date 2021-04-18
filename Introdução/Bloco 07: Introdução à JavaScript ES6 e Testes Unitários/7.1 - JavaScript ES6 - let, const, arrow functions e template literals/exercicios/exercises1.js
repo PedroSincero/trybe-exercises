995a046a7003b49ba470b566def9d4bd1e135055
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-04-18 13:47:08 -0300

    exercicios 7.1

---
+++ Introdução/Bloco 07: Introdução à JavaScript ES6 e Testes Unitários/7.1 - JavaScript ES6 - let, const, arrow functions e template literals/exercicios/exercises1.js
@@ -0,0 +1,44 @@
+//Const             Retira Parenteses add. =>
+function testingScope(escopo) {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(true);
+
+//Gabarito 
+const testingScope = escopo => { 
+    if (escopo === true) { 
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+  }
+
+  testingScope(true);
+//Part 1 - Exe: 2 
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  const ordemCrescente = () => {
+    oddsAndEvens[0] = 2;
+    oddsAndEvens[1] = 3;
+    oddsAndEvens[2] = 4;
+    oddsAndEvens[3] = 7;
+    oddsAndEvens[4] = 10;
+    oddsAndEvens[5] = 13;
+
+    return oddsAndEvens;
+  }
+  const ordem = ordemCrescente();
+
+  console.log(`Os números ${ordem} se encontram ordenados de forma crescente!`);
