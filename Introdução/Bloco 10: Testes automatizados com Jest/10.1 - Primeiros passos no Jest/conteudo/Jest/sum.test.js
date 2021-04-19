0f9adb02b81250366a2e103798e2fbfb55b89a17
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-04-19 17:26:08 -0300

    adicionando conteudo de jest

--- Introdução/Bloco 10: Testes automatizados com Jest/10.1 - Primeiros passos no Jest/conteudo/Jest/sum.test.js
+++ Introdução/Bloco 10: Testes automatizados com Jest/10.1 - Primeiros passos no Jest/conteudo/Jest/sum.test.js
@@ -1,5 +1,8 @@
-const sum = (a, b) => a + b;
+// sum.test.js
+const sum = require('./sum');
 
 test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
+  expect(sum(2, 3)).toBe(5);
 });
+
+//A linha module.exports = sum exporta a função sum no primeiro arquivo para que possa ser utilizada em outros módulos. No segundo arquivo, utilizamos require('./sum') para importar a função sum . Veja a seção de recursos adicionais para entender mais sobre como importar e exportar módulos em Node.js .
