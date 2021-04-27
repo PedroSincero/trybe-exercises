e7168ed3a8398f04754090bc1016adc48e6100f5
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-04-27 13:58:57 -0300

    adicionando ajustes 11.1

---
+++ Front-end/Bloco11-Introducao-React/11.1-IntroducaoFrontEnd/exercicios/teste-app/src/App.js
@@ -0,0 +1,12 @@
+import HelloMyFriends from "./exercises/HelooMyFriends";
+
+function App() {
+  return (
+    <div>
+    <h1>Hello my friends</h1>
+    <HelloMyFriends/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Front-end/Bloco11-Introducao-React/11.1-IntroducaoFrontEnd/exercicios/teste-app/src/exercises/HelooMyFriends.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HelloMyFriends() {
+    return (
+        
+        <p>
+            Hello meus Amigos!!
+        </p>
+        
+    )
+}
+
+export default HelloMyFriends;
