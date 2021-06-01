a79799bde2f384147e91cbbfb38a70fc0643c614
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-31 23:20:40 -0300

    adicionando o conteudo mapdispatch

--- Front-end/Bloco16-GerenciamentoDeEstadoComRedux/16.2-UsandoReduxNoReact/my-app/src/5 - mapDispatchToPropsEDispatch/FirstComponent.js
+++ Front-end/Bloco16-GerenciamentoDeEstadoComRedux/16.2-UsandoReduxNoReact/my-app/src/5 - mapDispatchToPropsEDispatch/FirstComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
+// 1 - importar o connect
 import { connect } from 'react-redux';
+// 1 - importar o index do action
 import { newAction } from './actions';
 
 class FirstComponent extends React.Component {
@@ -22,8 +24,8 @@ class FirstComponent extends React.Component {
     );
   }
 }
-
+// 2 - chamar o mapDispatchToProps
 const mapDispatchToProps = dispatch => ({
   myFirstDispatch: state => dispatch(newAction(state))});
-
+// 3 - chamar o connect adicionando o mapdispatch no segundo parametro
 export default connect(null, mapDispatchToProps)(FirstComponent);
