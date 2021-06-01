82b9c007711dedf7794ad6ac75519b95ec24eeaa
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-06-01 13:35:02 -0300

    adicionando exercicio 12.2

---
+++ Front-end/Bloco12-ComponentesComEstado-Eventos-e-Formularios-com-React/12.2-FormulariosNoReact/exercicio/formulario/src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      name: '',
+      email: '',
+      age: '',
+      anecdote: '',
+      terms: false,
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = (target.type === 'checkbox') ? target.checked : target.value
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
+        <form className="form">
+          <label>
+            Nome:
+            <input
+              name="name"
+              type="text"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              name="email"
+              type="email"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label>
+            Idade:
+            <select
+              name="age"
+              defaultValue=""
+              onChange={ this.handleChange }
+            >
+              <option value="">Selecione</option>
+              <option value="adult">Maior que 18</option>
+              <option value="underage">Menor que 18</option>
+            </select>
+          </label>
+          <label>
+            Anedota:
+            <textarea
+              name="anecdote"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="terms"
+              onChange={ this.handleChange }
+            />
+            Concordo com termos e acordos
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
